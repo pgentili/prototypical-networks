f66476fba489f618f48ae24a84a6beb3ca6dfcb5
Paolo Gentili <pgentili@mit.edu>
2019-02-26 11:25:31 -0500

    resnet scripts

--- 5_shot.sh
+++ 5_shot.sh
@@ -5,4 +5,4 @@
 #SBATCH --qos=high
 #SBATCH -o logs/5_shot.sh.log-%j
 
-python scripts/train/few_shot/run_train.py --data.dataset miniimagenet --data.split ravi --data.way 20 --model.x_dim '3,84,84' --data.cuda --log.exp_dir 5_shot_results
+python scripts/train/few_shot/run_train.py --data.dataset miniimagenet --data.split ravi --data.way 20 --model.x_dim '3,84,84' --data.cuda --log.exp_dir 5_shot_resnet_16_1_results --model.model_name protonet_resnet
